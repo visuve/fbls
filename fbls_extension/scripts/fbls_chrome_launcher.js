737ff3ddcf3e8be3488c98532182ba9efec08d89
visuve <veli-matti.visuri@iki.fi>
2019-01-28 11:56:16 +0200

    Add source

---
+++ fbls_extension/scripts/fbls_chrome_launcher.js
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env node
+
+try {
+	const cromeLaunch = require('chrome-launch');
+	const path = require('path');
+	const extensionSource = path.join(process.cwd(), process.argv[2]);
+	const arguments = ["--temp-profile", `--load-extension=${extensionSource}`];
+
+	cromeLaunch("about:blank", arguments);
+} catch (exception) {
+	console.error(`Script failed ${exception}`);
+}
---
+++ fbls_extension/scripts/fbls_svg_to_png.js
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env node
+
+// Example script arguments: source\fbls.svg
+try {
+	const sharp = require("sharp");
+	const path = require('path');
+	const inputFilePath = path.join(process.cwd(), process.argv[2]);
+	const inputFilePathObj = path.parse(inputFilePath);
+
+	for (size of [16, 32, 48, 96, 128]) {
+		let outPutFilePath = path.join(inputFilePathObj.dir, `${inputFilePathObj.name}_${size}.png`);
+		sharp(inputFilePath).resize({ width: size, height: size }).toFile(outPutFilePath, (error, info) => {
+			console.error(error);
+			console.error(info);
+		});
+	}
+} catch (exception) {
+	console.error(`Script failed ${exception}`);
+}
